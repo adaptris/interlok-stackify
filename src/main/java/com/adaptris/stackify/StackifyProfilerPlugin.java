310af6c8bb8a29797d916369ccae9794776399a8
Sebastien Belin <sebastien.belin@adaptris.com>
2018-09-18 03:19:37 +0100

    Fix some compilation error. Unfortunately there is still one in StatsCompiler that I cannot fix.

--- src/main/java/com/adaptris/stackify/StackifyProfilerPlugin.java
+++ src/main/java/com/adaptris/stackify/StackifyProfilerPlugin.java
@@ -6,18 +6,20 @@ import com.stackify.metric.MetricManager;
 public abstract class StackifyProfilerPlugin implements ClientPlugin {
 
   @Override
-  public void start() {
+  public void start(Object object) {
   }
 
   @Override
-  public void stop() {
+  public void stop(Object object) {
     MetricManager.shutdown(); // force a flush of any queued metrics.
   }
 
   @Override
-  public void init() {}
+  public void init(Object object) {
+  }
 
   @Override
-  public void close() {}
+  public void close(Object object) {
+  }
 
 }
